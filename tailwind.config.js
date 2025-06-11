cc8704963b2ab8e34beba1ce0bd1095b9f5e1585
gakusugino <g-sugino@macro-eg.co.jp>
2025-06-11 11:42:46 +0900

    TailWindCSS基礎学習

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#ebf8ff",
+          300: "#90cdf4",
+          500: "#4299e1",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
